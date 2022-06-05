b4278c36b291f7762c4c3c2b6a92fab0146453f0
Martins Ndifon <martinsndifon@gmail.com>
2022-06-05 12:33:04 -0700

    print numbers

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/* more headers goes there */
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-/* betty style doc for function main goes there */
+* main - Entry point
+*
+* Return: 0
+*
+*/
 
 int main(void)
 {
