2c2edb67d1c2f2a2f753fa556180dab229bf8901
Martins Ndifon <martinsndifon@gmail.com>
2022-06-05 11:24:17 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for(int x='A'; x <= 'Z'; x++)
+	{
+		int lower_x = tolower(x);
+		putchar(lower_x);
+	}
+	return (0);
+}
