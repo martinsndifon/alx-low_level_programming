d109b9e1ba23d5bf5e876810dce0516a2bbf258a
Martins Ndifon <martinsndifon@gmail.com>
2022-06-05 11:53:19 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int i;
+	{
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	for (i = 65; i < 91; i++)
+		putchar (i);
+	}
+
+	putchar(10);
+	return (0);
+}
