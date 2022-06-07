b2ce193783970b02b7b913181e8bf505ddb11355
Martins Ndifon <martinsndifon@gmail.com>
2022-06-07 13:05:33 -0700

    Trial for custom function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - using _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123;  i++)
+		_putchar(i);
+
+	_putchar(10);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+/**
+ * _putchar - write character to stdout
+ * int - the return type
+ * char - argument of type char
+ * Return: 0
+ */
+
+int _putchar(char);
+
+/* print_alphabet - print lowercase alphabet*/
+void print_alphabet(void);
