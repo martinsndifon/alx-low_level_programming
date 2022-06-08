edc5c46e2312520d5999d1fd4a73a368bfcca70d
Martins Ndifon <martinsndifon@gmail.com>
2022-06-07 19:04:58 -0700

    final push

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n >= 0; && n <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -27,7 +27,7 @@ void print_times_table(int n)
 					{
 						_putchar(' ');
 						_putchar(' ');
-						_putvhar(k + '0');
+						_putchar(k + '0');
 					} else if (k >= 10 && k < 100)
 					{
 						_putchar(' ');
@@ -45,5 +45,3 @@ void print_times_table(int n)
 		}
 	}
 }
-
-
