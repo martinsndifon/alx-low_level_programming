d57005b3f4e975e5b5be54816bd7f9b953dfa77d
Martins Ndifon <martinsndifon@gmail.com>
2022-06-10 05:53:48 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,9 +12,9 @@ void print_diagonal(int size)
 
 	if (size <= 0)
 		_putchar('\n');
-	for (a = 1; a <= size; a++)
+	for (a = 1; a <=size; a++)
 	{
-		for (b = 1; b <= size; b++)
+		for (b = 1; b <=size; b++)
 		{
 			_putchar('#');
 		}
