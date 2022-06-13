da92280b95862fc3018adb15f9554923faaa445e
Martins Ndifon <martinsndifon@gmail.com>
2022-06-13 14:35:09 -0700

    First main commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value of an int to 98
+ *
+ * return: void
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	n = &n;
+	*n = 98;
+	
+	return (0);
+}
