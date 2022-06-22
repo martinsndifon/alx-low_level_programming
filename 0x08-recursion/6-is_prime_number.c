459a4d1ae592239a4f28cf486a44a265bb136506
Martins Ndifon <martinsndifon@gmail.com>
2022-06-22 03:42:10 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,16 +10,10 @@
 
 int is_prime_number(int n)
 {
-	int i, j = 0;
-
-	for (i = 1; i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			j++;
-		}
-	}
-	if (j == 2)
+	int i;
+	
+	i = is_prime_number(n, n / 2);
+	if (i == 1)
 	{
 		return (1);
 	}
