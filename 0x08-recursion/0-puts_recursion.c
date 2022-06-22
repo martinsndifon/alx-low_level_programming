5e2c3f06426d514693c5e74d2fbe657edf42d282
Martins Ndifon <martinsndifon@gmail.com>
2022-06-22 04:36:08 -0700

    square root

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -17,5 +17,5 @@ void _puts_recursion(char *s)
 		_putchar(*(s + i));
 		i++;
 	}
-	_putchar('\n');
+	_putchar(10);
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,6 +14,10 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
+	else if (n == 1)
+	{
+		return (1);
+	}
 	else
 	{
 		return (_sqrt_recursion(n));
