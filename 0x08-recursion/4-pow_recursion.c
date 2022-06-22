1953f0b451c8b4b2cac131b533c08b55c3892159
Martins Ndifon <martinsndifon@gmail.com>
2022-06-22 03:22:58 -0700

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -17,8 +17,10 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	else
+	else (y >= 0)
 	{
-		return (x * _pow_recursion(x, y));
+		if (y == 0)
+			return (1);
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
