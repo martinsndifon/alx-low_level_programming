bce8b9d396ec6ab58b154cd8a60eaa4c0edd2836
Martins Ndifon <martinsndifon@gmail.com>
2022-06-29 04:53:30 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,7 +10,7 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, length;
+	int i, j, k, len;
 	char *str;
 
 	if (ac == 0 || av == NULL)
@@ -19,11 +19,11 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
-			length++;
-		length++;
+			len++;
+		len++;
 	}
 
-	str = malloc(sizeof(char) * (length + 1));
+	str = malloc(sizeof(char) * (len + 1));
 
 	if (str == NULL)
 		return (NULL);
