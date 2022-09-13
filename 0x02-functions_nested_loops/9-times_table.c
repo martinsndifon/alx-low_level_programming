791fd4547daf55f64647a9e131df6681ff6f6262
martinsndifon <martinsndifon@gmail.com>
2022-09-13 23:06:53 +0100

    Fixed incorrect output

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,16 +10,17 @@
 void times_table(void)
 {
 	int i, j, k;
-
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-			
-			if ((k <= 9) && (k != 0))
+			if (k <= 9 && j != 0)
 			{
 				_putchar(' ');
+			}
+			if (k <= 9)
+			{
 				_putchar(k + '0');
 			}
 			if (k > 9)
