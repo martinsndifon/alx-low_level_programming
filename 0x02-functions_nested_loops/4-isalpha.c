f42543be405f0101a6a1efe042b261f5eea8bf95
martinsndifon <martinsndifon@gmail.com>
2022-09-13 21:36:44 +0100

    Fix description for return value

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,12 @@
 /**
  * _isalpha - checks if an entered character is an alphabet
  * @c: character to check
- * return: 1 for alphabet and 0 for other characters
+ * Return: 1 for alphabet and 0 for other characters
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
