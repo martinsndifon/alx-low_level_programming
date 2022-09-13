bdfc396e2f3a6c79c77de2ce9a6cdb4d39c430b6
martinsndifon <martinsndifon@gmail.com>
2022-09-13 21:25:23 +0100

    Fix return values

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
  * _islower - checks if an entered character is lower case
- * @c - character to check
+ * @c: character to check
  * return: 0
  */
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
