0efcc2ba01a10ecd5e1794b4451002fd40d4ede2
martinsndifon <martinsndifon@gmail.com>
2022-09-19 21:48:16 +0100

    Readable version to 9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i;
 
-	while (*(src + i))
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		*(dest + i) = *(src + i);
-		i++;
+		dest[i] = src[i];
 	}
-	*(dest + i) = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
