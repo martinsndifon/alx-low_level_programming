b82c36124ecc1e0cd8fbc8f37cbf5f332d9bb408
martinsndifon <martinsndifon@gmail.com>
2022-09-19 21:47:00 +0100

    correction to 9-strcpy file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; src[i] >= '\0'; i++)
+	while (*(src + i))
 	{
-		dest[i] = src[i];
+		*(dest + i) = *(src + i);
+		i++;
 	}
+	*(dest + i) = '\0';
 	return (dest);
 }
