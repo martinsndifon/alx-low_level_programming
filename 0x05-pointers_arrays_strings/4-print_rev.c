3c79949aeee9e7d0f248cf77184b40aed51c6a4a
martinsndifon <martinsndifon@gmail.com>
2022-09-19 14:59:02 +0100

    Readable code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,14 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s + i))
+	while (s[i] != '\0')
+	{
 		i++;
+	}
 	i = i - 1;
 	while (i >= 0)
 	{
-		_putchar(*(s + i));
+		_putchar(s[i]);
 		i--;
 	}
 	_putchar('\n');
