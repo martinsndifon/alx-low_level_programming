c0fad2570c2965647e5e34a92fd67a6c5403539e
martinsndifon <martinsndifon@gmail.com>
2022-09-28 17:41:37 +0100

    Updated to cleaner code

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,15 +12,8 @@
 int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n == 0)
-	{
+	if (n == 0)
 		return (1);
-	}
-	else
-	{
-		return (n * factorial(n - 1));
-	}
+	return (n * factorial(n - 1));
 }
