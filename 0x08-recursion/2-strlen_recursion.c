88ddf40e87ec70c2de55d3a0fcdfe8e790d73042
martinsndifon <martinsndifon@gmail.com>
2022-09-28 14:34:06 +0100

    Check alternative

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,9 +9,11 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	static int i = 0;
+	if (*s != '\0')
 	{
-		return (0);
+		_strlen_recursion(s + 1);
+		i++;
 	}
-	return (1 + _strlen_recursion(++s));
+	return (i);
 }
