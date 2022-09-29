a757753f71c1b8977d74017e0300c88673e9ef23
martinsndifon <martinsndifon@gmail.com>
2022-09-29 10:32:48 +0100

    A more correct logic for finding prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,21 +10,14 @@
 
 int is_prime_number(int n)
 {
-	int i;
-
-	i = (n % 2);
-
 	if (n == 1)
-	{
 		return (0);
-	}
-
-	else if (i == 1)
-	{
+	if (n == 2)
 		return (1);
-	}
-	else
-	{
+	if (n % 10 == 5 && n != 5)
 		return (0);
-	}
+	if (n % 2 == 1)
+		return (1);
+	else
+		return (0);
 }
