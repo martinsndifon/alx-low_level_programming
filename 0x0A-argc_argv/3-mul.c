dea4f044ff9f8f1017e1b7d2b0ef14e1c434074d
martinsndifon <martinsndifon@gmail.com>
2022-10-01 12:08:02 +0100

    Revised edition of the code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,21 +10,10 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, k, result;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			j = atoi(argv[1]), k = atoi(argv[2]);
-			result = j * k;
-		}
-		printf("%d\n", result);
-	}
-	return (0);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 }
