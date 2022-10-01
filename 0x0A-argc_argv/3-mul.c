72477508968ea48bec22fe3c951307bee1cc8d93
martinsndifon <martinsndifon@gmail.com>
2022-10-01 12:34:38 +0100

    Best version of this function

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,12 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, k;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
-	k = i * j;
-	printf("%d\n", k);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
 }
