f2302947b49362df529602f175cdb4cafb38e3c7
Martins Ndifon <martinsndifon@gmail.com>
2022-10-12 14:56:22 +0000

    update to print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * print_name - prints name using another function
  * @name: the name to print
@@ -8,6 +9,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name);
-		(*f)(name);
+	if (name != NULL)
+		(*f)(name);;;
 }
