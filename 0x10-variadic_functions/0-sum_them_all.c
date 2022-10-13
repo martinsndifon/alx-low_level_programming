aa18958405cb5f61a1916bd6efdce5f6bc8d355e
Martins Ndifon <martinsndifon@gmail.com>
2022-10-13 10:11:26 +0000

    update to sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - a function that returns the sum of all its parameters
+ * @n: number of variable arguments to sum
+ *
+ * Return: (sum of the variable arguments)
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	unsigned int i;
+	va_list ap;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+		sum = 0;
+		for (i = 0; i < n; i++)
+			sum += va_arg(ap, int);
+	va_end(ap);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+Variadic functions are functions that take a variable number of arguments
+in declaration and definition. This directory describes how to declare variadic 
+functions, how to write them, and how to call them properly with examples in code.
