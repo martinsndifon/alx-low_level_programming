e7e55fd6449859be64c2a502e19fbddef5308fab
Martins Ndifon <martinsndifon@gmail.com>
2022-10-20 17:59:42 +0000

    betty correction to lack of description

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * list_len - counts the number of nodes in a linked list
+ * @h: pointer to the linked list
+ *
+ * Return: the lentgh of linked list
+ */
+
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
