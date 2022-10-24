6e043bd654b83002cf5d56cc37ef9578186764e5
Martins Ndifon <martinsndifon@gmail.com>
2022-10-24 20:06:27 +0000

    compiles without errors

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,11 @@
 
 size_t print_listint(const listint_t *h)
 {
-	long unsigned int i = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
