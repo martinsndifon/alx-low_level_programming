dbb5807abbd7e144169d38e5f6d0c44afe0d5a04
Martins Ndifon <martinsndifon@gmail.com>
2022-10-24 22:54:37 +0000

    initial commit for 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a listint_t list
+ * @head: pointer to the first node in the list
+ *
+ * Return: void
+ */
+
+ void free_listint2(listint_t **head)
+{
+
