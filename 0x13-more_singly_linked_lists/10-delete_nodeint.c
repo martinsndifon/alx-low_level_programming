1b006d4146d1c1b509828e965c11303e3a35706e
Martins Ndifon <martinsndifon@gmail.com>
2022-10-25 15:49:46 +0000

    update to 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free (temp);
+		free(temp);
 	}
 	else
 	{
@@ -40,7 +40,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		nextnode = temp->next;
 		temp->next = nextnode->next;
-		free (nextnode);
+		free(nextnode);
 	}
 	return (1);
 }
