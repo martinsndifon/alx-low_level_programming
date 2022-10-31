df63af037b3a24cf6aab0cf9f644b6f014f22766
Martins Ndifon <martinsndifon@gmail.com>
2022-10-31 17:02:06 +0000

    update to 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,9 +22,8 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[i] != '\0')
+	while (*(text_content + i) != '\0')
 		i++;
-	i += 1;
 
 	wfd = write(fd, text_content, i);
 	if (wfd == -1)
