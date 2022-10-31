0ecb18a097a9145b5e9a6184dfd2902039f951a9
Martins Ndifon <martinsndifon@gmail.com>
2022-10-31 16:17:20 +0000

    initial commit for 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * int create_file(const char *filename, char *text_content) - a function that creates a file.
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, rfd, wfd, i = 0;
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	
+	while (text_content[i] != NULL)
+		i++;
+	i += 1;
+
+	wfd = write(fd, text_content, i);
+	if (wfd == -1)
+		return (-1);
+
+	return (1);
+}
