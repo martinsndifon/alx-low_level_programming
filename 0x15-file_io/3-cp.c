f3135ddde8a22df16d6dbf738a7236cc4e185fe7
Martins Ndifon <martinsndifon@gmail.com>
2022-10-31 20:00:35 +0000

    update to 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,17 +18,13 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-
 	fd_to_read = open(argv[1], O_RDONLY);
-
 	if (fd_to_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_to_write = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	while ((n = read(fd_to_read, buf, 1024)) > 0)
 	{
 		if (fd_to_write < 0 || write(fd_to_write, buf, n) != n)
@@ -38,16 +34,13 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (n == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	rfd_f = close(fd_to_read);
 	rfd_t = close(fd_to_write);
-
 	if (rfd_f < 0 || rfd_t < 0)
 	{
 		if (rfd_f < 0)
@@ -56,6 +49,5 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to_write);
 		exit(100);
 	}
-
 	return (0);
 }
