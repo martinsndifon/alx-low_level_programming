518c5f069220c3ec3b14b1fa02186165116fa439
Martins Ndifon <martinsndifon@gmail.com>
2022-10-31 15:15:49 +0000

    fixed betty error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * ssize_t read_textfile(const char *filename, size_t letters) - a function that reads
- * a text file and prints it to the POSIX standard output.
+ * ssize_t read_textfile(const char *filename, size_t letters) - a function 
+ * that reads a text file and prints it to the POSIX standard output.
  * @filename: name of the text file to read from
  * @letters: the number of letters it should read and print
  *
  * Return: the actual number of letters it could read and print
- * 	   0 if the file can not be opened or read
- * 	   0 if filename is NULL
- * 	   0 if write fails or does not write the expected amount of bytes
+ * 0 if the file can not be opened or read
+ * 0 if filename is NULL
+ * 0 if write fails or does not write the expected amount of bytes
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
