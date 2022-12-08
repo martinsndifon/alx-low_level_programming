7e5a247188ed4770a3af48b181f7199c5a3c04d9
Martins Ndifon <martinsndifon@gmail.com>
2022-12-08 20:59:01 +0000

    update to 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for(; idx != 1; idx--)
+	for (; idx != 1; idx--)
 	{
 		if (!temp || !temp->next)
 			return (NULL);
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!node)
 	{
 		free(node);
-		return(NULL);
+		return (NULL);
 	}
 
 	node->n = n;
