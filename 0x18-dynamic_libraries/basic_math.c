baf886963dfec5b5240079c8b58d12e2bc5e2e5b
Martins Ndifon <martinsndifon@gmail.com>
2022-12-12 13:30:55 +0000

    update to 100-operations.so

---
+++ 0x18-dynamic_libraries/basic_math.c
@@ -0,0 +1,68 @@
+#include "math.h"
+
+/**
+ * add - adds two numbers
+ * @a: integer to compute
+ * @b: integer to compute
+ *
+ * Return: the sum of two integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: integer to compute
+ * @b: integer to compute
+ *
+ * Return: the difference of two integers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: integer to compute
+ * @b: integer to compute
+ *
+ * Return: the product of two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: integer to compute
+ * @b: integer to compute
+ *
+ * Return: the quotient of two integers
+ *
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - return the remainder of two numbers
+ * @a: integer to compute
+ * @b: integer to compute
+ *
+ * Return: remainder of a and b
+ *
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "math.h"
+
+int main()
+{
+	int sum;
+
+	sum = add(5,7);
+	printf("%d\n", sum);
+}
